10865f6dd8a272d61d961eae5c62f4843664e49f
Kaktys868 <lastochka1230@yandex.ru>
2025-12-23 08:36:13 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using APIGigaChat_True.Models.Response;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIGigaChat_True
+{
+    public class Program
+    {
+        static string ClientId = "019b493f-6c86-7084-8ea6-3ca097ab1023";
+        static string AuthorizationKey = "MDE5YjQ5M2YtNmM4Ni03MDg0LThlYTYtM2NhMDk3YWIxMDIzOjA4NTYyZDJiLTI5NjYtNDk4Mi1iYWNmLWY4N2I1NWYxMTlkOQ==";
+        static void Main(string[] args)
+        {
+        }
+        public static async Task<string> GetToken(string rqUID, string bearer)
+        {
+            string ReturnToken = null;
+            string Url = "https://ngw.devices.sberbank.ru:9WU3/api/v2/oauth";
+
+            using (HttpClientHandler Handler = new HttpClientHandler())
+            {
+                Handler.ServerCertificateCustomValidationCallback = (message, cert, chain, ss1PolicyErrors) => true;
+
+                using (HttpClient Clien = new HttpClient(Handler))
+                {
+                    HttpRequestMessage Request = new HttpRequestMessage(HttpMethod.Post, Url);
+
+                    Request.Headers.Add("Accept", "application/json");
+                    Request.Headers.Add("RqUID", rqUID);
+                    Request.Headers.Add("Authorization", $"Bearer {bearer}");
+
+                    var Data = new List<KeyValuePair<string, string>> {
+                        new KeyValuePair<string, string>("scope", "GIGACHAT_API_PERS")
+                    };
+
+                    Request.Content = new FormUrlEncodedContent(Data);
+
+                    HttpResponseMessage Response = await Clien.SendAsync(Request);
+
+                    if (Response.IsSuccessStatusCode)
+                    {
+                        string ResponseContent = await Response.Content.ReadAsStringAsync();
+                        ResponseToken Token = JsonConvert.DeserializeObject<ResponseToken>(ResponseContent);
+                        ReturnToken = Token.access_token;
+                    }
+                }
+            }
+
+            return ReturnToken;
+        }
+    }
+}
